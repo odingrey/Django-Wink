4ca9615325bb13bd5474a013c0a4fe7d9e8a931e
= <=>
2016-01-26 03:09:00 +0000

    "Fixed" login redirect issues, not happy with the fix though... will implement something stronger later

--- wink/wink/views.py
+++ wink/wink/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.shortcuts import render
@@ -31,6 +32,27 @@ def main(request):
 		}
 	    	return render(request, 'main.html', content)
 	return HttpResponseRedirect('/login')
+
+
+def loginUser(request):
+	if request.user.is_authenticated():
+		return HttpResponseRedirect('/')
+	else:
+		if request.method == 'POST':
+		        username = request.POST['username']
+		        password = request.POST['password']
+		
+		        user = authenticate(username=username, password=password)
+			if user is not None:
+		            if user.is_active:
+		                login(request, user)
+		                return HttpResponse('202') #Accepted
+		            else:
+		                return HttpResponse('403') #Forbidden (IE, account is banned)
+		        else:
+		            return HttpResponse('401') #Unauthorized (IE, bad username and password)
+		else:
+			return render(request, 'login.html')
 
 
 def register(request):
